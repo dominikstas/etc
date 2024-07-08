2dc004c4698cf42b65836dbb6c61659e2c99d464
HlukaszH <thelucasholecek@gmail.com>
2024-07-08 18:48:11 +0200

    dark/light mode toggle local memory upgrade, style fix

--- dlmode.js
+++ dlmode.js
@@ -1,6 +1,28 @@
 const toggleButton = document.getElementById('dark-mode-toggle');
 
-toggleButton.addEventListener('click', () => {
+// Function to initialize dark/light mode based on user preference
+function initializeDarkMode() {
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+    if (isDarkMode) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+}
+
+// Function to toggle between dark and light mode
+function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
     document.body.classList.toggle('light-mode');
-});
+
+    // Save dark mode preference to local storage
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    localStorage.setItem('darkMode', isDarkMode.toString());
+}
+
+// Event listener for dark/light mode toggle button
+toggleButton.addEventListener('click', toggleDarkMode);
+
+// Initialize dark/light mode on page load
+initializeDarkMode();
