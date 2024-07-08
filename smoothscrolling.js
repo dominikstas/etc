3fb07910b220070bf5c096c04ec2a819076d7e5f
HlukaszH <thelucasholecek@gmail.com>
2024-07-08 17:48:31 +0200

    dark mode fix, js script separation from html, cleaning

---
+++ smoothscrolling.js
@@ -0,0 +1,14 @@
+// JavaScript for smooth scrolling
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
