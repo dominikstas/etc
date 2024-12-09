447a332133d801227f7218fdbf395903ddff48be
Dominik Stasiak <d.stasiak436@gmail.com>
2024-12-09 08:07:17 +0100

    fixing blog styles bug

--- astro/src/components/BlogList.jsx
+++ astro/src/components/BlogList.jsx
@@ -1,13 +1,44 @@
 import React from "react";
 
 const BlogList = ({ posts }) => {
+  const styles = {
+    blogList: {
+      listStyleType: 'none',
+      padding: 0
+    },
+    blogItem: {
+      marginBottom: '1.5rem'
+    },
+    blogItemTitle: {
+      fontSize: '1.5rem',
+      margin: 0
+    },
+    blogItemExcerpt: {
+      color: '#777',
+      margin: '0.5rem 0 0'
+    },
+    blogItemLink: {
+      textDecoration: 'none',
+      color: '#000',
+      transition: 'color 0.3s'
+    },
+    blogItemLinkHover: {
+      color: '#007acc'
+    }
+  };
+
   return (
-    <ul className="blog-list">
+    <ul style={styles.blogList}>
       {posts.map((post) => (
-        <li key={post.id} className="blog-item">
-          <a href={post.slug}>
-            <h2>{post.title}</h2>
-            <p>{post.excerpt}</p>
+        <li key={post.id} style={styles.blogItem}>
+          <a 
+            href={post.slug} 
+            style={styles.blogItemLink}
+            onMouseEnter={(e) => e.target.style.color = styles.blogItemLinkHover.color}
+            onMouseLeave={(e) => e.target.style.color = styles.blogItemLink.color}
+          >
+            <h2 style={styles.blogItemTitle}>{post.title}</h2>
+            <p style={styles.blogItemExcerpt}>{post.excerpt}</p>
           </a>
         </li>
       ))}
@@ -16,35 +47,3 @@ const BlogList = ({ posts }) => {
 };
 
 export default BlogList;
-
-// Style w Astro lub CSS Modules
-<style>
-  .blog-list {
-    list-style: none;
-    padding: 0;
-  }
-
-  .blog-item {
-    margin-bottom: 1.5rem;
-  }
-
-  .blog-item h2 {
-    font-size: 1.5rem;
-    margin: 0;
-  }
-
-  .blog-item p {
-    color: #777;
-    margin: 0.5rem 0 0;
-  }
-
-  .blog-item a {
-    text-decoration: none;
-    color: #000;
-    transition: color 0.3s;
-  }
-
-  .blog-item a:hover {
-    color: #007acc;
-  }
-</style>
